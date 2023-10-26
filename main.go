4b93c727cbbfecb3b12600a093bf5c1f05c60897
Yuvraj Singh <ysrishu1031@gmail.com>
2023-10-26 21:19:49 +0530

    chat-app backend

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/YuvrajSingh3110/go-chat/pkg/webSocket"
+)
+
+func serveWS(pool *webSocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("websocket end point reached")
+
+	conn, err := webSocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Println("Error in upgrade")
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &webSocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := webSocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Chat app")
+	setupRoutes()
+	http.ListenAndServe(":5000", nil)
+}
